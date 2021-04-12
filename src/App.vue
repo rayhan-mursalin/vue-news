1574cf9654139bb0ed9fe87ecb0980c1c1c5678f
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-12 00:54:08 -0400

    Add template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<Layout>
-		<p>Main content goes here</p>
+		<h2 class="mb-8 text-4xl font-bold text-center capitalize">
+			News Section : <span class="text-green-700">{{ section }}</span>
+		</h2>
+		<NewsFilter v-model="section" :fetch="fetchNews" />
+		<NewsList :posts="posts" />
 	</Layout>
 </template>
 
@@ -16,6 +20,12 @@ export default {
 		Layout,
 		NewsFilter,
 		NewsList
+	},
+	data() {
+		return {
+			section: "home",
+			posts: [],
+		}
 	},
 }
 </script>
