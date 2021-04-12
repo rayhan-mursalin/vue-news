55c3b0fe31a4701d99343021910d4021d94f1935
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-12 01:11:19 -0400

    Update copyright

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
     class="px-4 py-8 text-sm font-bold text-center text-green-100 bg-green-900">
-    <p class="text-sm tracking-wide">Copyright (c) 2021</p>
+    <p class="text-sm tracking-wide">Copyright © 2021</p>
   </footer>
 </template>
