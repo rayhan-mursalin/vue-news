91b883b4c46e765eae6278207622a8897faad9c2
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-12 00:52:19 -0400

    Add Axios API

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,15 @@
 
 <script>
 import Layout from "./components/Layout.vue"
+import NewsFilter from "./components/NewsFilter.vue"
 import NewsList from "./components/NewsList.vue"
+import axios from "axios"
 
 export default {
 	name: 'App',
 	components: {
 		Layout,
+		NewsFilter,
 		NewsList
 	},
 }
