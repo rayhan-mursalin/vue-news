16cefcb883d7235c42a0ac38fe9317689a95da27
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-12 00:35:38 -0400

    Fix tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>News App</div>
+	<Layout>
+		<p>Main content goes here</p>
+	</Layout>
 </template>
 
 <script>
+import Layout from "./components/Layout.vue"
 
 export default {
-  name: 'App',
-  components: {
-  }
+	name: 'App',
+	components: {
+		Layout,
+	},
 }
 </script>
-
-<style>
-</style>
