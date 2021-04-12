67de8754a75ac2f0c5f659a166f3192340d7a19c
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-12 00:56:50 -0400

    Get news from API

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import Layout from "./components/Layout.vue"
 import NewsFilter from "./components/NewsFilter.vue"
 import NewsList from "./components/NewsList.vue"
 import axios from "axios"
+const api = 'pwxKoDMnFt3cCGLpALZTAnw7ocv9f5WZ';
 
 export default {
 	name: 'App',
@@ -27,5 +28,23 @@ export default {
 			posts: [],
 		}
 	},
+	methods: {
+			async fetchNews() {
+				try {
+					const url = `https://api.nytimes.com/svc/topstories/v2/${this.section}.json?api-key=${api}`
+					const response = await axios.get(url)
+					const results = response.data.results
+				} catch (err) {
+					if (err.response) {
+						// client received an error response (5xx, 4xx)
+						console.log("Server Error:", err)
+					} else if (err.request) {
+						// client never received a response, or request never left
+						console.log("Network Error:", err)
+					} else {
+						console.log("Client Error:", err)
+					}
+				}
+		},
 }
 </script>
